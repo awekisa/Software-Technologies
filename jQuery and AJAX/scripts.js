dc1cfe92675f6a9f9f6a0ae688479b1d337d2067
awekisa <dimitar.valtchev@gmail.com>
2016-06-30 19:41:18 +0300

    JQuery and Ajax lab and exercises

---
+++ jQuery and AJAX/scripts.js
@@ -0,0 +1,12 @@
+
+    function selectTowns() {
+        let li = $(this);
+        if (li.attr('data-selected')) {
+            li.removeAttr('data-selected')
+            li.css('background', '')
+        }
+        else {
+            li.attr('data-selected', 'true')
+            li.css('background', '#DDD')
+        }
+    }
